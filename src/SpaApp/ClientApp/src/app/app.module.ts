6100f8f907de2f2776fdaf8fe89a2104395bccb7
Israel <israel.nicacio@outlook.com>
2020-05-18 12:43:18 +0000

    Adicionado componente produto-lista

--- src/SpaApp/ClientApp/src/app/app.module.ts
+++ src/SpaApp/ClientApp/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home/home.component';
 import { LoginComponent } from './controleAcesso/login/login.component';
 import { AppRoutingModule } from './routes/app-routing/app-routing.module';
+import { ProdutoListaComponent } from './produtos/produto-lista/produto-lista.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    ProdutoListaComponent
   ],
   imports: [
     BrowserModule,
