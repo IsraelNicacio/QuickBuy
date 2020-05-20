e615ecfba0df7fda27b408f13e1364b556e8d181
Israel <israel.nicacio@outlook.com>
2020-05-20 00:37:44 +0000

    Validações no componente login

--- src/SpaApp/ClientApp/src/app/controleAcesso/login/login.component.ts
+++ src/SpaApp/ClientApp/src/app/controleAcesso/login/login.component.ts
@@ -9,19 +9,13 @@ import { Pessoa } from 'src/app/modelos/pessoa';
 
 export class LoginComponent implements OnInit {
   public pessoa;
-  public usuarioAutenticado: boolean;
 
   constructor() { 
     this.pessoa = new Pessoa();
   }
 
   entrar() {
-    if(this.pessoa.email == "israel@nicacio.com" && this.pessoa.senha == "123"){
-      this.usuarioAutenticado = true;
-    }
-    else{
-      this.usuarioAutenticado = false;
-    }
+    
     
   }
 
